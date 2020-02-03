9c6cac13a8798be98c6f6f299d9daacd8a185530
mauricio ramirez rodriguez <mauri6_6@live.com>
2020-02-02 21:28:40 -0600

    bye texto

--- MartanyProyecto/Login.aspx.cs
+++ MartanyProyecto/Login.aspx.cs
@@ -11,8 +11,7 @@ namespace MartanyProyecto
     {
         protected void Page_Load(object sender, EventArgs e)
         {
-            //hola bb
-            //Hola putillo
+            
         }
     }
 }
